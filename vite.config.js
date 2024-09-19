3e5ac6f6e1f203f5cccfda9dc6dad9c9b73620b1
ragedhuaman96 <ragedhuaman@gmail.com>
2024-09-18 19:23:31 -0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import jsconfigPaths from 'vite-jsconfig-paths'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), jsconfigPaths()],
+  resolve: {
+    alias: {
+      'components': path.resolve(__dirname, './src/components'),
+      'hooks': path.resolve(__dirname, './src/hooks'),
+      'pages': path.resolve(__dirname, './src/pages'),
+      'assets': path.resolve(__dirname, './src/assets'),
+      'adapters': path.resolve(__dirname, './src/adapters'),
+      'src/redux': path.resolve(__dirname, './src/redux'),
+      'services': path.resolve(__dirname, './src/services'),
+      'server': path.resolve(__dirname, './src/server'),
+      'utilities': path.resolve(__dirname, './src/utilities'),
+      'helpers': path.resolve(__dirname, './src/helpers'),
+      'containers': path.resolve(__dirname, './src/containers'),
+      'config': path.resolve(__dirname, './src/config'),
+    },
+  },
+})
